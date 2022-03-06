496c2bab528d9b5c8c8a1142ca7ff3fb53db98d5
nguyentranngoctrong <trongb1906792@student.ctu.edu.vn>
2022-03-06 19:23:02 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const setupContactRoutes = require("./app/routes/contact.routes");
-
 const { BadRequestError, errorHandler } = require("./app/errors");
 
-//module.exports = handlePromise;
-
 const app = express();
-setupContactRoutes(app);
 app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
+    res.json({
+        message: "Welcome to contact book application."
+    });
 });
+const setupContactRoutes = require("./app/routes/contact.routes");
+setupContactRoutes(app);
+
+// handle 404 response
 app.use((req, res, next) => {
-    // Code ở đây sẽ chạy khi không có route được định nghĩa nào
-    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
     next(new BadRequestError(404, "Resource not found"));
-    });
-    // define error-handling middleware last, after other app.use() and routes calls
-    app.use((err, req, res, next) => {
-    // Middleware xử lý lỗi tập trung.
-    // Trong các đoạn code xử lý ở các route, gọi next(error)
-    // sẽ chuyển về middleware xử lý lỗi này
+});
+
+app.use((err, req, res, next) => {
     errorHandler.handleError(err, res);
-    });
+});
 
 module.exports = app;
---
+++ app/routes/contact.routes.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+const contacts = require("../controllers/contact.controller");
+module.exports = (app) => {
+    const router = express.Router();
+   // Retrieve all contacts
+    router.get("/", contacts.findAll);
+   // Create a new contact
+    router.post("/", contacts.create);
+    // Delete all contacts
+    router.delete("/", contacts.deleteAll);
+   // Retrieve alL favorite contacts
+    router.get("/favorite", contacts.findAllFavorite);
+   // Retrieve a single contact with id
+    router.get("/:id", contacts.findOne);
+   // Update a contact with id
+    router.put ("/:id", contacts.update);
+   // DeLete a contact with id
+    router.delete("/:id", contacts.delete);
+    app.use("/api/contacts", router);
+};
